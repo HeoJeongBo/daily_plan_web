97ff9908dd3d646a0ef1d07a2373fa5ce31768dd
heojeongbo <heojungbo@gmail.com>
2021-08-01 01:38:26 +0900

    회원가입 폼 작성

--- src/components/main/LoginButton.tsx
+++ src/components/main/LoginButton.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler } from "react";
+import React from "react";
 import { History } from "history";
 import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
 import paths from "../../constants/path";
--- src/components/main/SingUp.tsx
+++ src/components/main/SingUp.tsx
@@ -1,40 +1,161 @@
-import React from "react";
+import {
+    Button,
+    createStyles,
+    Grid,
+    TextField,
+    makeStyles,
+    Theme,
+} from "@material-ui/core";
+import React, { ChangeEvent, useState } from "react";
 import { RouteComponentProps } from "react-router-dom";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            width: "100vw",
+            height: "100vh",
+            position: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+        },
+        title: {
+            textAlign: "center",
+            lineHeight: "2rem",
+        },
+        root: {
+            width: "360px",
+            padding: "30px 20px",
+            borderRadius: "20px",
+            border: "1px solid black",
+            "& .MuiTextField-root": {
+                "& .MuiOutlinedInput-input": {
+                    padding: "15.5px 14px",
+                },
+            },
+            "& .MuiInputBase-input": {
+                height: "1.3em",
+            },
+        },
+        btn: {
+            width: "100%",
+            borderRadius: "20px",
+            height: "45px",
+        },
+    })
+);
+
+type FormFields = {
+    email: string;
+    password: string;
+    confirmPassword: string;
+};
 
 const SignUp: React.FC<RouteComponentProps> = ({
     history,
 }: RouteComponentProps) => {
-    const renderSignUpForm = (): JSX.Element => {
+    const classes = useStyles();
+
+    const [fieldsValue, setFieldsValue] = useState<FormFields>({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const setField = (event: ChangeEvent<HTMLInputElement>): void => {
+        setFieldsValue({
+            ...fieldsValue,
+            [event.target.id]: event.target.value,
+        });
+    };
+
+    const renderTitle = (): JSX.Element => {
         return (
-            <div className="sign-up-form-container">
-                {renderID()}
-                {renderPassword()}
-                {renderConfirmPassword()}
-                {renderSubmitBtn()}
+            <div className={classes.title}>
+                <h1>매일 알림</h1>
+                <h2>회원 가입</h2>
             </div>
         );
     };
 
-    const renderID = (): JSX.Element => {
-        return <div className="id-container">ID</div>;
+    const renderEamil = (): JSX.Element => {
+        return (
+            <Grid item>
+                <TextField
+                    fullWidth
+                    variant="outlined"
+                    label="Email"
+                    id="email"
+                    onChange={(e) =>
+                        setField(e as ChangeEvent<HTMLInputElement>)
+                    }
+                />
+            </Grid>
+        );
     };
 
     const renderPassword = (): JSX.Element => {
-        return <div className="password-container">Password</div>;
+        return (
+            <Grid item>
+                <TextField
+                    fullWidth
+                    variant="outlined"
+                    label="Password"
+                    id="password"
+                    onChange={(e) =>
+                        setField(e as ChangeEvent<HTMLInputElement>)
+                    }
+                />
+            </Grid>
+        );
     };
 
     const renderConfirmPassword = (): JSX.Element => {
         return (
-            <div className="confirm-password-container">Confirm password</div>
+            <Grid item>
+                <TextField
+                    fullWidth
+                    variant="outlined"
+                    label="Confirm Password"
+                    id="confirmPassword"
+                    onChange={(e) =>
+                        setField(e as ChangeEvent<HTMLInputElement>)
+                    }
+                />
+            </Grid>
         );
     };
 
-    const renderSubmitBtn = (): JSX.Element => {
-        return <div className="submit-btn-container">회원가입</div>;
+    const renderSignUpBtn = (): JSX.Element => {
+        return (
+            <Grid item>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className={classes.btn}
+                >
+                    회원가입
+                </Button>
+            </Grid>
+        );
     };
 
-    console.log("asdasd");
-    return <div className="sign-up-container">{renderSignUpForm()}</div>;
+    return (
+        <div className={classes.container}>
+            <Grid
+                container
+                direction="column"
+                className={classes.root}
+                spacing={3}
+            >
+                {renderTitle()}
+                {renderEamil()}
+                {renderPassword()}
+                {renderConfirmPassword()}
+                {renderSignUpBtn()}
+            </Grid>
+        </div>
+    );
 };
 
 export default SignUp;
